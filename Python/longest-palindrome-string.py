eda455d24ba15962e3b45db86b73c1e87bab07fb
Keyvan Khosravian <keyvan.khosravian@gmail.com>
2025-09-21 16:48:09 -0700

    longest palindrom string

---
+++ Python/longest-palindrome-string.py
@@ -0,0 +1,10 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+"""
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+ 
