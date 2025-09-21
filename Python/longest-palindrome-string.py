fe07fa09bf4c9e5b4fa028b3b8d63de27fcead84
Keyvan Khosravian <keyvan.khosravian@gmail.com>
2025-09-21 16:49:31 -0700

    longest palindrom string 2

--- Python/longest-palindrome-string.py
+++ Python/longest-palindrome-string.py
@@ -2,9 +2,30 @@
 Given a string s, return the longest palindromic substring in s.
 """
 class Solution(object):
+
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
- 
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[1:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length palindrome
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[1:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
