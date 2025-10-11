8ee35f3ca13172a111328626a35148f6e90df80f
Keyvan Khosravian <keyvan.khosravian@gmail.com>
2025-10-10 19:51:16 -0700

    kth smallest element

---
+++ Python/Kth-smallest-element.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: Optional[TreeNode]
+        :type k: int
+        :rtype: int
+        """       
+        def visit(node, result):
+            if not node: return []
+
+            visit(node.left, result)
+            result.append(node)
+            visit(node.right, result)
+            
+            return result
+
+        return visit(root, [])[k - 1].val
