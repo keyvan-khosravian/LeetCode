120525fe646566e272b3d9652189b1fef234f723
Keyvan Khosravian <keyvan.khosravian@gmail.com>
2025-10-18 15:17:44 -0700

    permutations code

---
+++ Python/permutations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 1:
+            return [nums[:]]
+
+        result = []
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms: perm.append(n)
+            result.extend(perms)
+            nums.append(n)
+
+        return result
